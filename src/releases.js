f4b008f54269f4294d97ab9b2931648e1cd836c2
Allan Hortle <allanhortle@gmail.com>
2017-06-01 09:37:54 +1000

    Inital commit

---
+++ src/releases.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+import chalk from 'chalk';
+import ora from 'ora';
+
+import Request from './Request';
+import Loader from './Loader';
+
+function query(owner, name) {
+    return `{
+        repository(owner: "${owner}", name: "${name}") {
+            releases(last: 100) {
+                edges {
+                    node {
+                        tag {
+                            name
+                        }
+                        name
+                        description
+                    }
+                }
+            }
+        }
+    }`;
+};
+
+export default function modules(program, config) {
+    Loader.start();
+
+    const [owner, name] = program.args[1].split('/');
+    Request({query: query(owner, name)})
+        .then(({data}) => {
+            var list = data.repository.releases.edges
+                .map(ii => ii.node)
+                .map(({tag, name, description}) => {
+                    var title = chalk.yellow(`${name} - ${tag.name}`);
+
+                    return `${title}${description ? '\n' : ''}${description}`;
+                })
+                .join('\n\n');
+            Loader.stop();
+            console.log(list);
+        });
+}
