f4552ed541951accaba16240559d70947d328621
Allan Hortle <allanhortle@gmail.com>
2019-01-29 07:47:25 +1100

    add: wider debug, global logger

--- src/core/CoreScreen.js
+++ src/core/CoreScreen.js
@@ -4,9 +4,18 @@ import blessed from 'neo-blessed';
 const screen = blessed.screen({
     autoPadding: true,
     smartCSR: true,
+    dockBorders: true,
+    debug: true,
     title: 'react-blessed hello world'
 });
 
+screen.debugLog.width = '100%';
+screen.debugLog.height = '100%';
+screen.debugLog.top = 0;
+screen.debugLog.left = 0;
+
 screen.key(['escape', 'q', 'C-c'], () => process.exit(0));
+
+global.log = (val) => screen.debug('Log:', val);
 
 export default screen;
